87e577ae7ad124424c19b0c62541b25b5118f7b1
Peter Borzov <peter@sowingo.com>
2014-03-06 19:26:56 -0500

    Some reports

--- 01 Anagrams/naive.py
+++ 01 Anagrams/naive.py
@@ -2,12 +2,13 @@ import collections
 import sys
 
 def anagram(string1, string2):
+    if not len(string1)==len(string2):
+        return False
     def create_counter(from_string):
         counter = collections.Counter()
         for symbol in from_string:
             counter[symbol] +=1
         return counter
-
     counter1 = create_counter(string1)
     counter2 = create_counter(string2)
     mentioned_symbols = set(counter1.keys()+counter2.keys())
