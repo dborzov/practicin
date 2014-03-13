2d77a868d5759be9a2ea74ea80eec22fd5080267
Peter Borzov <peter@sowingo.com>
2014-03-13 19:35:29 -0400

    Blah-blah

--- 03 Graphs with Finn and Jake/solution/scrapper.py
+++ 03 Graphs with Finn and Jake/solution/scrapper.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import requests
 import re
+import json
 
-with open("../problem/characters.html","rb") as html_page:
-    for line in html_page:
-        match = re.search('href="/wiki/([\w\_]+)"\s+title="([\w\s]+)"',line)
-        if match:
-            print "Url: ", match.group(1), "\nName: ", match.group(2)
+config = json.loads(open("../problem/consts.json","rb").read())
+
+def scrap_characters():
+    with open("../problem/characters.html","rb") as html_page:
+        for line in html_page:
+            match = re.search('href="/wiki/([\w\_]+)"\s+title="([\w\s]+)"',line)
+            if match:
+                print "Url: ", match.group(1), "\nName: ", match.group(2)
+
+r = requests.get(config['list_of_episodes_url'])
+for each in r.text.splitlines():
+    pass
