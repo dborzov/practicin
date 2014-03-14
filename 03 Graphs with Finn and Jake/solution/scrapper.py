1e6b64c0d6b151f2ff9382f3cf96c750888f2449
Peter Borzov <peter@sowingo.com>
2014-03-14 18:52:03 -0400

    Episodes scrapping

--- 03 Graphs with Finn and Jake/solution/scrapper.py
+++ 03 Graphs with Finn and Jake/solution/scrapper.py
@@ -13,6 +13,12 @@ def scrap_characters():
             if match:
                 print "Url: ", match.group(1), "\nName: ", match.group(2)
 
+
 r = requests.get(config['list_of_episodes_url'])
-for each in r.text.splitlines():
-    pass
+source_html = r.text.rstrip().encode('utf-8')
+print source_html.encode('utf-8')
+episode_entries =  re.findall(u'<tr style="text-align: center; background: #f2f2f2">([.\n]+)</tr>',source_html)
+
+for episode in episode_entries:
+    print 'Heyo!'
+    print episode.encode('utf-8')
