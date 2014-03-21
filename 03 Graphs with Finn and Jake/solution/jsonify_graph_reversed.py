6e9f6a19e10f36a43ecd941b7f0833d163fb30df
Peter Borzov <peter@sowingo.com>
2014-03-21 17:11:44 -0400

    Vertices graph

--- 03 Graphs with Finn and Jake/solution/jsonify_graph_reversed.py
+++ 03 Graphs with Finn and Jake/solution/jsonify_graph_reversed.py
@@ -7,12 +7,13 @@ for episode in open('episodes.csv','rb'):
     wiki, title = episode.split(' | ')
     episodes[wiki] = title.rstrip()
 
-characters = collections.defaultdict(list)
+graph = collections.defaultdict(list)
 for filename in os.listdir('graph'):
     episode = filename[:-4]
     print 'Reading episode:', episode
-    for character in open('graph/'+filename,'rb'):
-        characters[character.rstrip()].append(episodes[episode])
+    graph[episodes[episode]] = [character.rstrip() for character in open('graph/'+filename,'rb')] 
 
-json_file = open('graph.json','wb')
-json_file.write(json.dumps(characters,indent=4, separators=(',', ': ')))
+
+json_file = open('graph_vertices.json','wb')
+json_file.write(json.dumps(graph,indent=4, separators=(',', ': ')))
+
