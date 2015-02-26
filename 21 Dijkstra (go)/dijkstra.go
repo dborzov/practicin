1644dc39edd9b0bba838387cfc27162421c39a78
Borzov <tihoutrom@gmail.com>
2015-02-27 01:26:00 +0500

    Gotta make shit work

--- 21 Dijkstra (go)/dijkstra.go
+++ 21 Dijkstra (go)/dijkstra.go
@@ -1,10 +1,32 @@
 package dijkstra
 
+import "container/heap"
+
+type tuple struct{
+	v int // vertex index
+	d float64 // distance to the vertex
+}
+
+type DistQueue []tuple
+func (h DistQueue) Len() int           { return len(h) }
+func (h DistQueue) Less(i, j int) bool { return h[i].d < h[j].d }
+func (h DistQueue) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *DistQueue) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(tuple))
+}
+
+func (h *DistQueue) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 func Dijkstra(graph [][]float64, start int) []float64{
-	pq.Add(start, distances[start])
-
-
 	// initialize with infinitely high values
 	distances := make([]float64, len(graph))
 	for i:=0; i<len(distances); i++ {
@@ -13,22 +35,32 @@ func Dijkstra(graph [][]float64, start int) []float64{
 	distances[start] = 0.0
 
 	// initialize priority queue
-	pq := PriorityQueue()
+	pq := make(DistQueue, len(graph))
+	pqp := &pq
 	for i:=0; i<len(distances); i++ {
-		pq.Add(distances[i],i)
+		pq[i] = tuple{
+			v: i,
+			d: distances[i],
+		}
 	}
 
-	for !pq.IsEmpty() {
-		p := pq.Pop()
-		for j:= range p.Adjacent {
-			newDistance := distances[p] + graph[p.Adjacent[j]]
-			if newDistance < distances[p.Adjacent[j]] {
-				distances[p.Adjacent[j]] := newDistance
-				pq.UpdateKey(distances[p.Adjacent[j]],p.Adjacent[j]) 
+	heap.Init(pqp)
+	for pqp.Len() > 0 {
+		p := pqp.Pop().(tuple)
+		for j, d:= range graph[p.v] {
+			if d==0.0 {
+				// 0.0 denotes no edge
+				continue
+			}
+			newDistance := p.d + d
+			if newDistance < distances[j] {
+				distances[j] = newDistance
+				// pqp.UpdateKey(distances[p.Adjacent[j]],p.Adjacent[j]) 
 			} 
 		} 
 
 
 	} 
 
+	return []float64{}
 }
--- 21 Dijkstra (go)/dijkstra_test.go
+++ 21 Dijkstra (go)/dijkstra_test.go
@@ -1,20 +1,23 @@
 package dijkstra
 
-import ("testing")
+import ("testing" 
+		"fmt"
+
+)
 
 const (
-	Seattle iota
+	Seattle=iota
 	SF
 	NY
 	Waterloo
 )
 
-func TestDijkstra(t testing.T) {
-	var graph [4][4]float64
-	graph[Seattle] = [...]float64{0,2,0,6}
-	graph[SF] = [...]float64{2,0,7,0}
-	graph[NY] = [...]float64{0,7,0,1}
-	graph[Waterloo] = [...]float64{6,0,1,0}
+func TestDijkstra(t *testing.T) {
+	var graph [][]float64
+	graph[Seattle] = []float64{0,2,0,6}
+	graph[SF] = []float64{2,0,7,0}
+	graph[NY] = []float64{0,7,0,1}
+	graph[Waterloo] = []float64{6,0,1,0}
 	
 	f := Dijkstra(graph, Waterloo)
 	fmt.Printf("distance matrix from Waterloo: %#v \n", f)
